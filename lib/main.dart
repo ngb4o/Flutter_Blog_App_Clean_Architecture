7c8d21499deaf6923b4c263d501b2f7a02efeef3
ngb4o <ngbao08052003@gmail.com>
2024-06-03 21:27:39 +0700

    committ

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog_app_clean_architecture/features/auth/presentation/pages/signup_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Debug App',
+      home: SignUpPage(),
+    );
+  }
+}
