ac320d6010547166583501415d5414c157424546
ngb4o <ngbao08052003@gmail.com>
2024-06-06 20:17:43 +0700

    connet supabase

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,4 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://cahceaoazbwgusisxctr.supabase.co';
+  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhaGNlYW9hemJ3Z3VzaXN4Y3RyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc2NzkxMTMsImV4cCI6MjAzMzI1NTExM30.biQdFBOJlt7qaCgA5z3TZTf4-k9-lupNFfDwadEs0wA';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog_app_clean_architecture/core/secrets/app_secrets.dart';
 import 'package:flutter_blog_app_clean_architecture/core/theme/theme.dart';
 import 'package:flutter_blog_app_clean_architecture/features/auth/presentation/pages/login_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  //Supabase
+  WidgetsFlutterBinding.ensureInitialized();
+  final supabase = await Supabase.initialize(
+      url: AppSecrets.supabaseUrl,
+      anonKey: AppSecrets.supabaseAnonKey
+  );
   runApp(MyApp());
 }
 
