6d0cedf3dbf2483c65ea445c143b0b8d1b211c53
ngb4o <ngbao08052003@gmail.com>
2024-06-06 21:24:58 +0700

    data

--- lib/features/auth/data/repository/auth_repository_impl.dart
+++ lib/features/auth/data/repository/auth_repository_impl.dart
@@ -1,18 +1,39 @@
+import 'package:flutter_blog_app_clean_architecture/core/error/exceptions.dart';
 import 'package:flutter_blog_app_clean_architecture/core/error/failures.dart';
+import 'package:flutter_blog_app_clean_architecture/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:flutter_blog_app_clean_architecture/features/auth/domain/repository/auth_repository.dart';
-import 'package:fpdart/src/either.dart';
+import 'package:fpdart/fpdart.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource remoteDataSource;
+
+  AuthRepositoryImpl(this.remoteDataSource);
+
   @override
-  Future<Either<Failures, String>> loginWithEmailPassword({required String email, required String password}) {
+  Future<Either<Failures, String>> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
     // TODO: implement loginWithEmailPassword
     throw UnimplementedError();
   }
 
   @override
-  Future<Either<Failures, String>> signUpWithEmailPassword({required String name, required String email, required String password}) {
-    // TODO: implement signUpWithEmailPassword
-    throw UnimplementedError();
-  }
+  Future<Either<Failures, String>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userId = await remoteDataSource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
 
+      return right(userId);
+    } on ServerException catch (e) {
+      return left(Failures(e.message));
+    }
+  }
 }
