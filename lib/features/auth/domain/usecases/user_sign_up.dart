7191efaa62b34f0810b4646a87122de4fffe986e
ngb4o <ngbao08052003@gmail.com>
2024-06-09 07:16:45 +0700

    commit

--- lib/features/auth/domain/usecases/user_sign_up.dart
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -1,44 +1,32 @@
-// import 'package:flutter_blog_app_clean_architecture/core/error/failures.dart';
-// import 'package:flutter_blog_app_clean_architecture/features/auth/domain/repository/auth_repository.dart';
-// import 'package:fpdart/fpdart.dart';
-//
-// import '../../../../core/usecase/usecase.dart';
-//
-// class UserSignUp implements Usecase<String, UserSignUpParams> {
-//   final AuthRepository authRepository;
-//
-//   UserSignUp(this.authRepository);
-//
-//   @override
-//   Future<Either<Failures, String>> call(UserSignUpParams params) async {
-//     return await authRepository.signUpWithEmailPassword(
-//       name: params.name,
-//       email: params.email,
-//       password: params.password,
-//     );
-//   }
-// }
-//
-// class UserSignUpParams {
-//   final String email;
-//   final String name;
-//   final String password;
-//
-//   UserSignUpParams({
-//     required this.email,
-//     required this.name,
-//     required this.password,
-//   });
-// }
+import 'package:flutter_blog_app_clean_architecture/core/error/failures.dart';
+import 'package:flutter_blog_app_clean_architecture/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
 
-// class UserSignUpParams {
-//   final String email;
-//   final String name;
-//   final String password;
-//
-//   UserSignUpParams({
-//     required this.email,
-//     required this.name,
-//     required this.password,
-//   });
-// }
+import '../../../../core/usecase/usecase.dart';
+
+class UserSignUp implements Usecase<String, UserSignUpParams> {
+  final AuthRepository authRepository;
+
+  UserSignUp(this.authRepository);
+
+  @override
+  Future<Either<Failures, String>> call(UserSignUpParams params) async {
+    return await authRepository.signUpWithEmailPassword(
+      name: params.name,
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class UserSignUpParams {
+  final String email;
+  final String name;
+  final String password;
+
+  UserSignUpParams({
+    required this.email,
+    required this.name,
+    required this.password,
+  });
+}
